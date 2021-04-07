b4817dd28261a4a4cf0d2d6848ebb3a930e1bd37
nithish <>
2021-04-07 17:58:55 +0530

    {/src/main/java/com/myspace/testproject/brms.java}

--- src/main/java/com/myspace/testproject/brms.java
+++ src/main/java/com/myspace/testproject/brms.java
@@ -6,12 +6,23 @@ package com.myspace.testproject;
 
 public class brms implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public brms() {
-    }
+	private java.lang.String name;
 
+	public brms() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public brms(java.lang.String name) {
+		this.name = name;
+	}
 
 }
